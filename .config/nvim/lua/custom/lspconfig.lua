90a116d2acc19021eac1f976f5c343efad5b9bee
Rickard Rosen <rickardrosen@gmail.com>
2021-11-14 13:56:10 +0100

    terraform lsp default

--- .config/nvim/lua/custom/lspconfig.lua
+++ .config/nvim/lua/custom/lspconfig.lua
@@ -2,25 +2,15 @@ local M = {}
   M.setup_lsp = function(attach, capabilities) 
     local lspconfig = require "lspconfig"
     -- lspservers with default config
-    local servers = { "html", "tsserver" }
+    local servers = { "html", "tsserver", "terraformls" }
     for _, lsp in ipairs(servers) do    
-      lspconfig[lsp].setup {   
+      lspconfig[lsp].setup({   
         on_attach = attach,   
         capabilities = capabilities,    
         flags = {      
           debounce_text_changes = 150,   
         },  
-      } 
+      }) 
     end 
- 
-    lspconfig.terraformls.setup{
-      on_attach=custom_attach;
-      capabilities = capabilities;
-      flags = {
-          debounce_text_changes = 150,
-      };
-      filetypes = { "tf" };
-      cmd = {'terraform-ls', 'serve'}
-    }
   end
 return M
